f3eb9f0d3485fc07463574212684d5ed83e3707e
Zac Jones <zacjones93@gmail.com>
2023-02-09 16:48:23 -0500

    task: rebrand app

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      'background': '#e9f0ea',
+      'card-background': '#CCF2D4',
+      'primary': '#27D6B8',
+      'color-text': '#0c2e13',
+      'color-subtext': '#777d78',
+      'color-background': '#edf2ee',
+      'background-dark': '#323433',
+      'card-background-dark': '#5A5C5A',
+      'primary-dark': '#C47474',
+      'color-text-dark': '#EDF2EE',
+    },
     extend: {},
   },
   plugins: [],
